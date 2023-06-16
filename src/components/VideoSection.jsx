34f14575c0dcd443a4a51ae33910cb60d4d628a9
RafaelMiguel03 <miguel.rafaellouie2003@gmail.com>
2023-06-16 22:09:50 +0800

    fixed id problem

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -5,8 +5,11 @@ const VideoSection = () => {
   return (
     <div className='w-full h-auto bg-black p-5'>
       <h3 className='text-white text-center text-2xl font-bold pb-5'>Video Documentation</h3>
-      <div className='w-full  flex justify-center md:w-1/2 lg:w-1/3 xl:w-2/4 mx-auto'>
-        <video src={vid} autoPlay loop muted className='w-full h-auto' />
+      <div className='w-full flex justify-center md:w-1/2 lg:w-1/3 xl:w-2/4 mx-auto'>
+        <video controls className='w-full h-auto'>
+          <source src={vid} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
     </div>
   );
